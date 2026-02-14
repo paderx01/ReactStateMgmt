681ff9c47e9295d8f292ef42f83036e4be12162b
Pader <paderx26@gmail.com>
2026-02-14 11:06:35 -0600

    reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,62 @@
 import { useState } from "react";
 
-function NameList() {
-  const [list, setList] = useState(["Joe", "Jill", "Jack"]);
-  const [name, setName] = useState("");
-
-  const onAddName = () => {
-    setList([...list, name]);
-    setName("");
-  };
-
-  return (
-    <div>
-      <ul>
-        {list.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button onClick={onAddName}>A dd Name</button>
-    </div>
-  );
-}
-
-function Counter() {
-  const [count, setCount] = useState(10);
-
-  function addOne() {
-    setCount(count + 1);
-  }
-
-  return (
-    <div className="App">
-      <button onClick={addOne}> Count = {count}</button>
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <div>
-      <Counter />
-      <NameList />
-    </div>
-  );
+  return <div className="App">Using reducer</div>;
 }
 
 export default App;
+
+//UseState below:
+
+// import { useState } from "react";
+
+// function NameList() {
+//   const [list, setList] = useState(["Joe", "Jill", "Jack"]);
+//   const [name, setName] = useState(() => "Pader");
+
+//   const onAddName = () => {
+//     setList([...list, name]);
+//     setName("");
+//   };
+
+//   return (
+//     <div>
+//       <ul>
+//         {list.map((name) => (
+//           <li key={name}>{name}</li>
+//         ))}
+//       </ul>
+//       <input
+//         type="text"
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <button onClick={onAddName}>Add Name</button>
+//     </div>
+//   );
+// }
+
+// function Counter() {
+//   const [count, setCount] = useState(10);
+
+//   function addOne() {
+//     setCount(count + 1);
+//   }
+
+//   return (
+//     <div className="App">
+//       <button onClick={addOne}> Count = {count}</button>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Counter />
+//       <NameList />
+//     </div>
+//   );
+// }
+
+// export default App;
