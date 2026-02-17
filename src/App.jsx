79e1cb0c77c38d6737a9e7f03dd4792387bf3ad9
Pader <paderx26@gmail.com>
2026-02-17 13:28:38 -0600

    useReducer exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
 import { useReducer } from "react";
 
-function App() {
+function UserForm() {
+  const [state, dispatch] = useReducer(
+    (state, action) => ({
+      ...state,
+      ...action,
+    }),
+    {
+      first: "",
+      last: "",
+    },
+  );
+  return (
+    <div>
+      <input
+        type="text"
+        value={state.first}
+        onChange={(e) => dispatch({ first: e.target.value })}
+      />
+      <div>First: {state.first}</div>
+      <div>Last: {state.last}</div>
+      <input
+        type="text"
+        value={state.last}
+        onChange={(e) => dispatch({ last: e.target.value })}
+      />
+    </div>
+  );
+}
+
+function NameList() {
   const [state, dispatch] = useReducer(
     (state, action) => {
-      switch ((action, type)) {
-        case "setName":
+      switch (action.type) {
+        case "SET_NAME":
           return { ...state, name: action.payload };
+        case "ADD_NAME":
+          return {
+            ...state,
+            names: [...state.names, state.name],
+            name: "",
+          };
       }
     },
     {
@@ -15,6 +50,12 @@ function App() {
   );
   return (
     <div className="App">
+      <div>
+        {state.names.map((name, index) => (
+          <div key={index}>{name} </div>
+        ))}
+      </div>
+
       <input
         type="text"
         value={state.name}
@@ -22,8 +63,17 @@ function App() {
           dispatch({ type: "SET_NAME", payload: e.target.value })
         }
       />
+      <button onClick={() => dispatch({ type: "ADD_NAME" })}>Add Name</button>
     </div>
   );
+}
+
+function App() {
+  return;
+  <div>
+    <UserForm />
+    <NameList />
+  </div>;
 }
 
 export default App;
